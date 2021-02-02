f0f704f8f8d052029aba718e365d501e365e4a84
flemington123 <w1120309794@gmail.com>
2021-02-02 20:09:04 +0800

    Create lab5.js
    rename js file

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Xuqi Wang
+ * Created: 31 Jan 2021
+ * Licensse: Public Domain
+ */
+
+// Define Variabels
+make  = "Ford";
+model = "Taurus";
+color = "Black";
+year  = 2004;
+
+//calculate
+age = 2021 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
