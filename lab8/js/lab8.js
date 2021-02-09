25edfc08901115eaecb44c633d195411d3b587ea
flemington123 <w1120309794@gmail.com>
2021-02-10 00:38:39 +0800

    Update lab8.js

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -1,27 +1,45 @@
 /*
  * Author: Xuqi Wang
- * Created: 4 Feb 2021
+ * Created: 10 Feb 2021
  * Licensse: Public Domain
  */
 
 
 // Define Variables
-
-function sortUserName(){
-  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
-  console.log("userName =", userName);
-
-  var nameArray = userName.split('');
-  console.log("nameArray =", nameArray);
-
-  var nameArraySort = nameArray.sort();
-  console.log("nameArraySort =", nameArraySort);
-
-  var nameSorted = nameArraySort.join('');
-  console.log("nameSorted =", nameSorted);
-
-  return nameSorted;
+function firstThing(test){
+  console.log(test + ": This is the first thing.<br>")
 }
 
-//output
-document.writeln("Oh hey, I've fixed yoru name: ", sortUserName(), "</br>");
+function secondThing(test){
+  console.log(test + ": This is the second thing.<br>")
+}
+
+function thirdThing(test){
+  console.log(test + ": This is the third thing.<br>")
+}
+
+firstThing("TEST 1");
+secondThing("TEST 1");
+thirdThing("TEST 1");
+
+//Test 2
+setTimeout(function(){
+  firstThing("TEST 2");
+}, 0);
+setTimeout(function(){
+  secondThing("TEST 2");
+}, 0);
+setTimeout(function(){
+  thirdThing("TEST 2");
+}, 0);
+
+//Test 3
+setTimeout(function(){
+  firstThing("TEST 3");
+}, 3000);
+setTimeout(function(){
+  secondThing("TEST 3");
+}, 3000);
+setTimeout(function(){
+  thirdThing("TEST 3");
+}, 3000);
